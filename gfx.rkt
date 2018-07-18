347a12407b0d26debe66e102ab5f70bc3ea2b0c9
turab1996 <turab1996.tj@gmail.com>
2018-07-18 00:38:37 -0400

    Updated all functions to use racket/draw. Only fan-combine for deck left

--- gfx.rkt
+++ gfx.rkt
@@ -1,5 +1,5 @@
 (module gfx racket
-  (require "structs.rkt" racket/draw racket/gui (except-in 2htdp/image make-pen make-color))
+  (require "structs.rkt" racket/draw racket/gui (except-in 2htdp/image make-pen make-color beside #;empty-image))
   (provide #;card #;#;#;fan-combine deck coin)
   
   ;; Card dimensions
@@ -34,7 +34,7 @@
   (define (show-image bm)
     (make-object image-snip% bm))
 
-  ;; number: Int Color -> bitmap-dc%
+  ;; number: Int Color -> bitmap%
   ;; Creates an image of a number with the given color
   (define (number n c)
     (define num (number->string n))
@@ -48,7 +48,7 @@
     (send dc draw-text num (/ N-B-W 2) (/ N-B-W 2))
     target)
 
-  ;; card: Int Color -> Image
+  ;; card: Int Color -> bitmap%
   ;; Creates an image of a card
   (define (card n c)
     (define num (number n c))
@@ -71,28 +71,59 @@
   
   (define fanning-angle -10)
   
-  ;; fan-combine: Image Image -> Image
+  ;; empty-image: bitmap%
+  (define empty-image (make-bitmap 1 1))
+
+  ;; beside: Int bitmap% bitmap% -> bitmap%
+  ;; Combines the given bitmap objects with the given gap in between
+  (define (beside gap bmp1 bmp2)
+    (define bmp2-w (send bmp2 get-width))
+    (define bmp2-h (send bmp2 get-height))
+    (define bmp1-w (send bmp1 get-width))
+    (define bmp1-h (send bmp1 get-height))
+    (define target (make-bitmap (+ bmp2-w gap bmp1-w) (max bmp2-h bmp1-h)))
+    (define dc (new bitmap-dc% [bitmap target])) 
+    (send dc draw-bitmap bmp1 0 0)
+    (send dc draw-bitmap bmp2 (+ gap bmp1-w) 0)
+    target)
+
+  ;; center: bitmap% bitmap% -> bitmap%
+  ;; Places the first bitmap at the center of the second bitmap
+  (define (center bmp1 bmp2)
+    (define bmp2-w (send bmp2 get-width))
+    (define bmp2-h (send bmp2 get-height))
+    (define bmp1-w (send bmp1 get-width))
+    (define bmp1-h (send bmp1 get-height))
+    (define target (make-bitmap (max bmp1-w bmp2-w) (max bmp1-h bmp2-h)))
+    (define dc (new bitmap-dc% [bitmap target]))
+    (send dc draw-bitmap bmp2 0 0)
+    (send dc draw-bitmap bmp1 (- (/ bmp2-w 2) (/ bmp1-w 2)) (- (/ bmp2-h 2) (/ bmp1-h 2)))
+    target)
+
+  ;; fan-combine: bitmap% bitmap% -> bitmap%
   ;; Combines two images by the set fanning angle
-  #;(define (fan-combine img1 img2)
-    (define deck (overlay/xy img1 20 -5 (rotate fanning-angle img2)))
-    (rotate 0 #;(* -1 fanning-angle) deck))
-  
-  ;; deck: [ListOf Card] (Image Image -> Image) -> Image
+  (define (fan-combine img1 img2)
+    (define img2-w (send img2 get-width))
+    (define img2-h (send img2 get-height))
+    (send img2 rotate fanning-angle)
+    (send img2 draw-bitmap img1 20 -5))
+
+  ;; deck: [ListOf Card] (bitmap-% Image -> Image) -> Image
   ;; Returns an image of a deck
-  #;(define (deck loc comb-f)
+  (define (deck loc comb-f)
     (if (empty? loc) empty-image (comb-f (card (card-value (car loc))
-                                                (card-color (car loc)))
-                                          (deck (cdr loc) comb-f))))
+                                               (card-color (car loc)))
+                                         (deck (cdr loc) comb-f))))
 
   #;(overlay (rotate 20 (deck `(,(Card "red" 1)
-                   ,(Card "red" 2)
-                   ,(Card "red" 3)
-                   ,(Card "red" 4)
-                   ,(Card "red" 5)) fan-combine)) (empty-scene 510 170 "black"))
+                                ,(Card "red" 2)
+                                ,(Card "red" 3)
+                                ,(Card "red" 4)
+                                ,(Card "red" 5)) fan-combine)) (empty-scene 510 170 "black"))
 
   ;; Coin dimensions
-  (define CO-R 5)
-  (define CO-BORDER 2)
+  (define CO-R 10)
+  (define CO-BORDER 4)
 
   (define HINT-C "lightblue")
   (define HINT-C-BG "blue")
@@ -100,6 +131,11 @@
   (define BOMB-C-BG "tomato")
 
   ;; coin: Color Color -> Image
-  #;(define (coin fg bg)
-    (overlay (circle CO-R "solid" fg)
-             (circle (+ CO-R CO-BORDER) "solid" bg))))
+  (define (coin fg bg)
+    (define target (make-bitmap (* 2 CO-R) (* 2 CO-R)))
+    (define dc (new bitmap-dc% [bitmap target]))
+    (send dc set-brush fg 'solid)
+    (send dc set-pen bg CO-BORDER 'solid)
+    ;; for some reason, the circle does not fit when drawing starts from 0 0
+    (send dc draw-ellipse 2 2 (- (* 2 CO-R) CO-BORDER) (- (* 2 CO-R) CO-BORDER))
+    target))
